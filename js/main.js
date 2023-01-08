bcf231f24e130705b73633a35bb6a66244daf848
danimatanovic <danijeladacamat@gmail.com>
2023-01-08 07:39:42 +0100

    Added mobile header

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ $(".upcoming-events-slide").slick({
     infinity: true,
     speed: 2500,
 });
+function toggleBurger(i) {
+    const html = document.querySelector("html");
+    const body = document.querySelector("body");
+    const nav = document.querySelector(".nav-menu");
+    nav.classList.toggle("open");
+    i.classList.toggle("is-active");
+    html.classList.toggle("no-scroll");
+    body.classList.toggle("no-scroll");
+  }
